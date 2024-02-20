94934e0c3d5000a8bbcf047d4f1bc3b73fa58726
Britnay Gore <britnaygore268@gmail.com>
2024-02-20 14:58:41 -0500

    Get Authors and show on DOM when Authors button is clicked

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,8 +1,8 @@
 import { booksOnSale, getBooks } from '../api/bookData';
 import { signOut } from '../utils/auth';
 import { showBooks } from '../pages/books';
-// import { getAuthors } from '../api/authorData';
-// import { showAuthors } from '../pages/authors';
+import { getAuthors } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 
 // navigation events
 const navigationEvents = () => {
@@ -28,7 +28,7 @@ const navigationEvents = () => {
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
     console.warn('CLICKED AUTHORS');
-    // getAuthors().then(showAuthors);
+    getAuthors().then(showAuthors);
   });
 
   // STRETCH: SEARCH
